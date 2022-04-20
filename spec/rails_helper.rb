e8860ab5244c9dd8698f30a346225bbf8da0e62f
Reid Conway <rconway@uccs.edu>
2022-04-20 19:13:39 +0000

    Final for iteration 5

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,13 @@ SimpleCov.start 'rails' do
   add_filter '/bin/'
   add_filter '/db/'
   add_filter '/spec/' # for rspec
+
+  SimpleCov.start :rails do
+    filters.clear # This will remove the :root_filter and :bundler_filter that come via simplecov's defaults
+    add_filter do |src|
+      !(src.filename =~ /^#{SimpleCov.root}/) unless src.filename =~ /my_engine/
+    end
+  end
 end
 
 require'devise'
